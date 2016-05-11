dda5b816c400e4b8f0bce82f1b0d3744f414d5ea
Pooja <csitpooja@gmail.com>
2016-05-11 11:59:15 +0530

    latest commit

--- index.js
+++ index.js
@@ -3,16 +3,3 @@
  */
 var a="hello"
 var b;
-console.log("ruhi");
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("hello")
-console.log("neeraj test merge")
-console.log("neeraj test merge")
-console.log("neeraj test merge")
